3f60b202d876f3214080463444b9d99f065cf95a
tbence94 <tihanyibence@gmail.com>
2025-02-21 17:11:35 +0100

    implement facebook limited login with nonce
    (cherry picked from commit 222955aaaad9835b0153e6dce67cbe5276963d00)

--- packages/authentication/ios/Plugin/Handlers/FacebookAuthProviderHandler.swift
+++ packages/authentication/ios/Plugin/Handlers/FacebookAuthProviderHandler.swift
@@ -4,6 +4,7 @@ import FirebaseAuth
 #if RGCFA_INCLUDE_FACEBOOK
 import FBSDKLoginKit
 #endif
+import CryptoKit // Import for nonce generation
 
 class FacebookAuthProviderHandler: NSObject {
     let errorSignInCanceled = "Sign in canceled."
@@ -39,36 +40,76 @@ class FacebookAuthProviderHandler: NSObject {
         #if RGCFA_INCLUDE_FACEBOOK
         let scopes = call.getArray("scopes", String.self) ?? []
         DispatchQueue.main.async {
-            self.loginManager.logIn(permissions: ["email", "public_profile"] + scopes, from: self.pluginImplementation.getPlugin().bridge?.viewController) { result, error in
-                if let error = error {
+            // https://developers.facebook.com/docs/facebook-login/limited-login/faq
+            // In response to the upcoming changes to ATT enforcement, we made changes to the iOS SDK and the SDK
+            // no longer provides valid user access tokens in scenarios where the user opts out of ATT.
+            // The access token validation or Graph API requests may throw errors like OAuthException -
+            // “Invalid OAuth access token - Cannot parse access token”. Our recommendation is
+            // that users integrate Limited Login following the official documentation:
+
+            // See these for implementation details:
+            // https://firebase.google.com/docs/auth/ios/facebook-login#implement_facebook_limited_login
+            // https://developers.facebook.com/docs/facebook-login/limited-login/ios
+
+            let nonce = UUID().uuidString
+            let viewController = self.pluginImplementation.getPlugin().bridge?.viewController
+            let loginConfiguration = LoginConfiguration(
+                permissions: ["email", "public_profile"] + scopes,
+                tracking: .limited,
+                nonce: self.sha256(nonce)
+            )
+
+            self.loginManager.logIn(viewController: viewController, configuration: loginConfiguration) { result in
+                switch result {
+                case .failed(let error):
                     if isLink == true {
                         self.pluginImplementation.handleFailedLink(message: nil, error: error)
                     } else {
                         self.pluginImplementation.handleFailedSignIn(message: nil, error: error)
                     }
                     return
-                }
-
-                guard let accessToken = result?.token else {
+                case .cancelled:
                     if isLink == true {
                         self.pluginImplementation.handleFailedLink(message: self.errorLinkCanceled, error: nil)
                     } else {
                         self.pluginImplementation.handleFailedSignIn(message: self.errorSignInCanceled, error: nil)
                     }
                     return
-                }
+                case .success:
+                    guard let idTokenString = AuthenticationToken.current?.tokenString else {
+                        if isLink == true {
+                            self.pluginImplementation.handleFailedLink(message: self.errorLinkCanceled, error: nil)
+                        } else {
+                            self.pluginImplementation.handleFailedSignIn(message: self.errorSignInCanceled, error: nil)
+                        }
+                        return
+                    }
 
-                let accessTokenString = accessToken.tokenString
-                let credential = FacebookAuthProvider.credential(withAccessToken: accessTokenString)
-                if isLink == true {
-                    self.pluginImplementation.handleSuccessfulLink(credential: credential, idToken: nil, nonce: nil,
-                                                                   accessToken: accessTokenString, serverAuthCode: nil, displayName: nil, authorizationCode: nil)
-                } else {
-                    self.pluginImplementation.handleSuccessfulSignIn(credential: credential, idToken: nil, nonce: nil,
-                                                                     accessToken: accessTokenString, displayName: nil, authorizationCode: nil, serverAuthCode: nil)
+                    let credential = OAuthProvider.credential(withProviderID: "facebook.com",
+                                                               idToken: idTokenString,
+                                                              rawNonce: nonce)
+                    if isLink == true {
+                        self.pluginImplementation.handleSuccessfulLink(credential: credential, idToken: idTokenString, nonce: nonce,
+                                                                   accessToken: nil, serverAuthCode: nil, displayName: nil, authorizationCode: nil)
+                    } else {
+                        self.pluginImplementation.handleSuccessfulSignIn(credential: credential, idToken: idTokenString, nonce: nonce,
+                                                                     accessToken: nil, displayName: nil, authorizationCode: nil, serverAuthCode: nil)
+                    }
                 }
             }
         }
         #endif
     }
+
+    @available(iOS 13, *)
+    private func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        String(format: "%02x", $0)
+      }.joined()
+
+      return hashString
+    }
+
 }
